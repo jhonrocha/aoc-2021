b43131f45a634f319260edf18071f9eef5a17cdc
Jhon Rocha <jhmrocha@gmail.com>
2021-12-17 22:01:27 -0300

    Finishing Day 6!

--- src/day6.rs
+++ src/day6.rs
@@ -22,7 +22,14 @@ fn part1(path: &str, days: u64) -> u64 {
 // From TJ!
 fn part2(path: &str, days: u64) -> u64 {
     let mut count = VecDeque::from(vec![0; 9]);
-    unimplemented!();
+    let lines: Vec<Vec<u64>> = read_lines_chars(path, |c| c.to_digit(10).map(u64::from)).unwrap();
+    lines[0].iter().for_each(|&i| count[i as usize] += 1);
+    (0..days).for_each(|_| {
+        let born = count.pop_front().unwrap();
+        count.push_back(born);
+        count[6] += born;
+    });
+    count.iter().sum()
 }
 
 fn main() {
@@ -30,7 +37,10 @@ fn main() {
         "Results for Part1 are: {:?}",
         part1("fixtures/day6.txt", 80)
     );
-    // part2("fixtures/day6.txt", 80);
+    println!(
+        "Results for Part2 are: {:?}",
+        part2("fixtures/day6.txt", 256)
+    );
 }
 
 #[cfg(test)]
@@ -45,6 +55,8 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part1("fixtures/day6-test.txt", 256), 26984457539);
+        assert_eq!(part2("fixtures/day6-test.txt", 18), 26);
+        assert_eq!(part2("fixtures/day6-test.txt", 80), 5934);
+        assert_eq!(part2("fixtures/day6-test.txt", 256), 26984457539);
     }
 }
