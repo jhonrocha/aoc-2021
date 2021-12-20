9c5df70d085b25b1d3db2195a9909792634bf71f
Jhon Rocha <jhmrocha@gmail.com>
2021-12-20 20:12:51 -0300

    Day8: Done!

--- src/day8.rs
+++ src/day8.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashSet, str::FromStr, string::ParseError};
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 
 enum Digit {
     One,
@@ -37,6 +40,44 @@ fn read_outputs(path: &str) -> usize {
         .count()
 }
 
+fn deduce_signals(signals: Vec<&str>) -> HashMap<usize, HashSet<char>> {
+    let mut missing = Vec::<HashSet<char>>::new();
+
+    let mut mask: HashMap<usize, HashSet<char>> =
+        signals.iter().fold(HashMap::new(), |mut map, signal| {
+            match signal.len() {
+                2 => map.insert(1, signal.chars().collect::<HashSet<char>>()),
+                3 => map.insert(7, signal.chars().collect::<HashSet<char>>()),
+                4 => map.insert(4, signal.chars().collect::<HashSet<char>>()),
+                7 => map.insert(8, signal.chars().collect::<HashSet<char>>()),
+                _ => {
+                    missing.push(signal.chars().collect::<HashSet<char>>());
+                    None
+                }
+            };
+            map
+        });
+
+    for m in missing {
+        if m.len() == 6 {
+            if m.intersection(mask.get(&4).unwrap()).count() == 4 {
+                mask.insert(9, m);
+            } else if m.intersection(mask.get(&1).unwrap()).count() == 1 {
+                mask.insert(6, m);
+            } else {
+                mask.insert(0, m);
+            }
+        } else if m.intersection(mask.get(&1).unwrap()).count() == 2 {
+            mask.insert(3, m);
+        } else if m.intersection(mask.get(&4).unwrap()).count() == 3 {
+            mask.insert(5, m);
+        } else {
+            mask.insert(2, m);
+        }
+    }
+    mask
+}
+
 fn sonar_output(path: &str) -> usize {
     // let lines: Vec<Vec<&str>> =
     std::fs::read_to_string(path)
@@ -45,21 +86,18 @@ fn sonar_output(path: &str) -> usize {
         // .flat_map(|line| {
         .map(|line| {
             let entries = line.split('|').collect::<Vec<&str>>();
-            let signals = entries[0];
-            let a: Vec<HashSet<_>> = signals
-                .split_whitespace()
-                .map(|pat| pat.chars().collect::<HashSet<_>>())
-                .collect();
-            println!("{:?}", a);
-            let numbers = entries[1];
-            // .pop()
-            // .unwrap()
-            // .split_whitespace()
-            // .filter_map(|digit| digit.parse::<Digit>().ok())
-            0
+            let decoder = deduce_signals(entries[0].split_whitespace().collect());
+            let resp = entries[1].split_whitespace().fold(0, |mut acc, digit| {
+                let d_set: HashSet<char> = digit.chars().collect();
+                if let Some(value) = decoder.iter().find(|(_, val)| **val == d_set) {
+                    acc = acc * 10 + value.0
+                } else {
+                }
+                acc
+            });
+            resp
         })
-        .count();
-    0
+        .sum::<usize>()
 }
 
 fn part1(path: &str) -> usize {
@@ -72,7 +110,7 @@ fn part2(path: &str) -> usize {
 
 fn main() {
     println!("Results for Part1 are: {:?}", part1("fixtures/day8.txt"));
-    // println!("Results for Part2 are: {:?}", part2("fixtures/day8.txt"));
+    println!("Results for Part2 are: {:?}", part2("fixtures/day8.txt"));
 }
 
 #[cfg(test)]
@@ -86,17 +124,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2("fixtures/day8-test.txt"), 168);
+        assert_eq!(part2("fixtures/day8-test.txt"), 61229);
     }
-
-    // cf      1
-    // acf     7
-    // bcdf    4
-    // acdeg   2
-    // acdfg   3
-    // abdfg   5
-    // abcefg  0
-    // abdefg  6
-    // abcdfg  9
-    // abcdefg 8
 }
