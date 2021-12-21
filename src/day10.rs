612d620a8a22e0e56a1a82fac160854539e3930d
Jhon Rocha <jhmrocha@gmail.com>
2021-12-21 14:18:58 -0300

    Finsished Day10

--- src/day10.rs
+++ src/day10.rs
@@ -25,13 +25,16 @@ impl Signal {
         }
     }
 
-    fn get_value(&self) -> Option<usize> {
+    fn get_value(&self) -> usize {
         match self {
-            Signal::CloseBracket => Some(3),
-            Signal::CloseSquare => Some(57),
-            Signal::CloseCurly => Some(1197),
-            Signal::CloseAngle => Some(25137),
-            _ => None,
+            Signal::CloseBracket => 3,
+            Signal::CloseSquare => 57,
+            Signal::CloseCurly => 1197,
+            Signal::CloseAngle => 25137,
+            Signal::OpenBracket => 1,
+            Signal::OpenSquare => 2,
+            Signal::OpenCurly => 3,
+            Signal::OpenAngle => 4,
         }
     }
 }
@@ -54,39 +57,68 @@ impl FromStr for Signal {
     }
 }
 
+enum Diagnostic {
+    Corrupted(Signal),
+    Incomplete(Vec<Signal>),
+    Correct,
+}
+
+fn check_line(line: &[Signal]) -> Diagnostic {
+    let mut queue = Vec::<Signal>::new();
+    let corrupt = line.iter().find(|&sig| {
+        if let Some(open) = sig.match_open() {
+            if !queue.is_empty() {
+                open != queue.pop().expect("Must be a Signal")
+            } else {
+                true
+            }
+        } else {
+            queue.push(sig.clone());
+            false
+        }
+    });
+    if let Some(value) = corrupt {
+        Diagnostic::Corrupted(value.clone())
+    } else if !queue.is_empty() {
+        Diagnostic::Incomplete(queue)
+    } else {
+        Diagnostic::Correct
+    }
+}
+
 fn part1(path: &str) -> usize {
     read_lines_chars(path, |c| c.to_string().parse::<Signal>().ok())
         .unwrap()
         .iter()
-        .fold(0, |acc, line| {
-            let mut queue = Vec::<Signal>::new();
-            let value = line.iter().find(|&sig| {
-                if let Some(open) = sig.match_open() {
-                    if !queue.is_empty() {
-                        open != queue.pop().expect("Must be a Signal")
-                    } else {
-                        true
-                    }
-                } else {
-                    queue.push(sig.clone());
-                    false
-                }
-            });
-            if let Some(value) = value {
-                acc + value.get_value().unwrap()
-            } else {
-                acc
-            }
+        .fold(0, |acc, line| match check_line(line) {
+            Diagnostic::Corrupted(value) => acc + value.get_value(),
+            _ => acc,
         })
 }
 
 fn part2(path: &str) -> usize {
-    unimplemented!()
+    let mut scores = read_lines_chars(path, |c| c.to_string().parse::<Signal>().ok())
+        .unwrap()
+        .iter()
+        .filter_map(|line| match check_line(line) {
+            Diagnostic::Incomplete(missing) => {
+                Some(
+                    missing
+                        .iter()
+                        .rev()
+                        .fold(0, |acc, sig| acc * 5 + sig.get_value()),
+                )
+            }
+            _ => None,
+        })
+        .collect::<Vec<usize>>();
+    scores.sort_unstable();
+    scores[scores.len() / 2]
 }
 
 fn main() {
     println!("Results for Part1 are: {:?}", part1("fixtures/day10.txt"));
-    // println!("Results for Part2 are: {:?}", part2("fixtures/day10.txt"));
+    println!("Results for Part2 are: {:?}", part2("fixtures/day10.txt"));
 }
 
 #[cfg(test)]
@@ -98,8 +130,8 @@ mod tests {
         assert_eq!(part1("fixtures/day10-test.txt"), 26397);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2("fixtures/day10-test.txt"), 1134);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("fixtures/day10-test.txt"), 288957);
+    }
 }
