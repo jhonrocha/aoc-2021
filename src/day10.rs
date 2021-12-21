02e5de35b5ecc8b30c0ed1c6b56d4731de3b70c2
Jhon Rocha <jhmrocha@gmail.com>
2021-12-21 10:36:54 -0300

    Day10 - Part1: Done

--- src/day10.rs
+++ src/day10.rs
@@ -1,15 +1,92 @@
+use std::str::FromStr;
+
+use aoc::read_lines_chars;
+
+#[derive(Debug, PartialEq, Clone)]
+enum Signal {
+    OpenBracket,
+    OpenSquare,
+    OpenCurly,
+    OpenAngle,
+    CloseBracket,
+    CloseSquare,
+    CloseCurly,
+    CloseAngle,
+}
+
+impl Signal {
+    fn match_open(&self) -> Option<Self> {
+        match self {
+            Signal::CloseBracket => Some(Signal::OpenBracket),
+            Signal::CloseSquare => Some(Signal::OpenSquare),
+            Signal::CloseCurly => Some(Signal::OpenCurly),
+            Signal::CloseAngle => Some(Signal::OpenAngle),
+            _ => None,
+        }
+    }
+
+    fn get_value(&self) -> Option<usize> {
+        match self {
+            Signal::CloseBracket => Some(3),
+            Signal::CloseSquare => Some(57),
+            Signal::CloseCurly => Some(1197),
+            Signal::CloseAngle => Some(25137),
+            _ => None,
+        }
+    }
+}
+
+impl FromStr for Signal {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "(" => Ok(Signal::OpenBracket),
+            "[" => Ok(Signal::OpenSquare),
+            "{" => Ok(Signal::OpenCurly),
+            "<" => Ok(Signal::OpenAngle),
+            ")" => Ok(Signal::CloseBracket),
+            "]" => Ok(Signal::CloseSquare),
+            "}" => Ok(Signal::CloseCurly),
+            ">" => Ok(Signal::CloseAngle),
+            _ => Err("Wrong Signal".to_string()),
+        }
+    }
+}
+
 fn part1(path: &str) -> usize {
-    unimplemented!()
+    read_lines_chars(path, |c| c.to_string().parse::<Signal>().ok())
+        .unwrap()
+        .iter()
+        .fold(0, |acc, line| {
+            let mut queue = Vec::<Signal>::new();
+            let value = line.iter().find(|&sig| {
+                if let Some(open) = sig.match_open() {
+                    if !queue.is_empty() {
+                        open != queue.pop().expect("Must be a Signal")
+                    } else {
+                        true
+                    }
+                } else {
+                    queue.push(sig.clone());
+                    false
+                }
+            });
+            if let Some(value) = value {
+                acc + value.get_value().unwrap()
+            } else {
+                acc
+            }
+        })
 }
 
 fn part2(path: &str) -> usize {
     unimplemented!()
-
 }
 
 fn main() {
     println!("Results for Part1 are: {:?}", part1("fixtures/day10.txt"));
-    println!("Results for Part2 are: {:?}", part2("fixtures/day10.txt"));
+    // println!("Results for Part2 are: {:?}", part2("fixtures/day10.txt"));
 }
 
 #[cfg(test)]
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs::read_to_string, str::FromStr};
+use std::{error::Error, fs::read_to_string, str::FromStr, collections::VecDeque};
 
 pub fn read_lines<T>(path: &str) -> Result<Vec<T>, Box<dyn Error>>
 where
@@ -19,6 +19,17 @@ where
         .lines()
         .map(|l| l.chars().filter_map(&f).collect())
         .collect::<Vec<Vec<T>>>())
+}
+
+pub fn read_queue_chars<T, F>(path: &str, f: F) -> Result<Vec<VecDeque<T>>, Box<dyn Error>>
+where
+    T: FromStr,
+    F: Fn(char) -> Option<T>,
+{
+    Ok(read_to_string(path)?
+        .lines()
+        .map(|l| l.chars().filter_map(&f).collect())
+        .collect::<Vec<VecDeque<T>>>())
 }
 
 pub fn read_lines_split<T, F>(path: &str, f: F) -> Result<Vec<Vec<T>>, Box<dyn Error>>
