78081ab7b44c7baedb9ab22a0dc29daa65077aed
Jhon Rocha <jhmrocha@gmail.com>
2021-12-27 23:05:44 -0300

    Day16: Begining

---
+++ src/day16.rs
@@ -0,0 +1,17 @@
+
+
+fn part1(path: &str) {
+    
+}
+
+fn main() {
+    println!("Part1: {:?}", part1("fixtures/day16.txt"));
+}
+
+#[cfg(test)]
+mod day16 {
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("fixtures/day16-test.txt"), 10);
+    }
+}
